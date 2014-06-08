78f147b4b9fa3511067d545aae77dede8d9ddfcf
Kim Dantes <kimdantes@gmail.com>
2014-06-09 02:00:55 +0800

    approve event

--- app/modules/initiateevent/approve_event/view.js
+++ app/modules/initiateevent/approve_event/view.js
@@ -1,18 +1,30 @@
 'use strict';
 
 define(['app', 'Initiateevents'], function(app, Initiateevents) {
-    app.controller('ApproveViewCtrl', [
+    app.controller('ApproveEvent', [
         '$scope',
-        '$location', 
+        '$state',
         '$stateParams',
         'InitiateEventFactory',
-        function($scope, $location, $stateParams, InitiateEventFactory) {
+        function($scope, $state, $stateParams, InitiateEventFactory) {            
+            $scope.update = function(form) {            
+                var updateApprovalStatus = $scope.initiateevents;
+                InitiateEventFactory.update($scope.initiateevent, function(err) {
+                    if (err.errors) {
+                        for (var key in err.errors) {
+                            form[key].message = err.errors[key].message;
+                        }
+                    }else{
+                        $state.go('approve_event');
+                    }
+                });
+            };
+
             $scope.findOne = function() {
                 InitiateEventFactory.get({
-                    initiateEventId: $stateParams.initiateEventId      
-                               
+                    initiateEventId: $stateParams.initiateEventId
                 }, function(initiateevent) {
-                    $scope.initiateevent = initiateevent;              
+                    $scope.initiateevent = initiateevent;
                 });
             };
         }
