5850c28c4aff61eae098a4ea4b8d160f6be38046
Kim Dantes <kimdantes@gmail.com>
2014-06-12 21:52:30 +0800

    supplies add.html and .js

--- app/modules/prepare_event/reserve/supplies/add.js
+++ app/modules/prepare_event/reserve/supplies/add.js
@@ -16,18 +16,21 @@ define([
             SuppliesResourceFactory, 
             InitiateEventFactory
         ) {
+            $scope.initiateevent = {};
             $scope.reserved_supplies = []; //for initiate event reserved supplies array
             $scope.view = false;
 
-           
+            $scope.initiateevent.reserved_supplies = $scope.reserved_supplies;
+            
             $scope.submit = function(form){
+                //if (name) {
                     $scope.reserved_supplies.push({
-                        name: name,
-                        qty: qty
+                        name:name,
+                       //qtyreserved:qtyreserved
                     });
+                //} 
             };
-
-
+            
             $scope.all = function() {
                 
                 SuppliesResourceFactory.query(function(suppliesresources) {
