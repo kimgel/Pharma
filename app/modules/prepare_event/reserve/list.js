20e437633ad527fbf012ad5951e6f694b0a3916e
Kim Dantes <kimdantes@gmail.com>
2014-06-16 22:43:59 +0800

    supplies for purchase modified

--- app/modules/prepare_event/reserve/list.js
+++ app/modules/prepare_event/reserve/list.js
@@ -2,37 +2,26 @@
 
 define([
     'app',
-    'SuppliesResources',
-    'Initiateevents'
+    'Initiateevents',
 ], function(app, Initiateevents) {
-    app.controller('SelectedSupplies', [
+    app.controller('PrepareEventList', [
         '$scope',
         '$state',
-        'SuppliesResourceFactory',
         'InitiateEventFactory',
         function(
             $scope,
             $state,
-            SuppliesResourceFactory, 
             InitiateEventFactory
         ) {
-            $scope.initiateevent = {};
-            //$scope.reserved_supplies = []; //for initiate event reserved supplies array
-           // $scope.view = false;
 
-            //$scope.initiateevent.reserved_supplies = $scope.reserved_supplies;
-                        
             $scope.all = function() {
                 
-                /*SuppliesResourceFactory.query(function(suppliesresources) {
-                    $scope.suppliesresources = suppliesresources;
-                });*/
-
                 InitiateEventFactory.query(function(initiateevents) {
-                    $scope.reservedSupplies = initiateevents[0].reserved_supplies;
+                    $scope.initiateevents = initiateevents;
                 });
             };
 
+          
         }
     ]);
 
--- app/modules/prepare_event/reserve/supplies/list.js
+++ app/modules/prepare_event/reserve/supplies/list.js
@@ -1,17 +1,45 @@
-'use strict'; 
+'use strict';
 
-define(['app', 'Initiateevents'], function(app, Initiateevents) {
+define([
+    'app',
+    'SuppliesResources',
+    'Initiateevents'
+], function(app, Initiateevents) {
     app.controller('SelectedSupplies', [
         '$scope',
-        '$location',
-        'InitiateEventFactory', 
-        function($scope, $location, InitiateEventFactory) {
+        '$state',
+        'SuppliesResourceFactory',
+        'InitiateEventFactory',
+        function(
+            $scope,
+            $state,
+            SuppliesResourceFactory, 
+            InitiateEventFactory
+        ) {
+            $scope.initiateevent = {};
+            $scope.reserved_supplies = []; 
+            $scope.purchased_supplies = [];//for initiate event reserved supplies array
+            $scope.view = false;
+
+            $scope.initiateevent.reserved_supplies = $scope.reserved_supplies;
+            
+            
             $scope.all = function() {
                 
+                SuppliesResourceFactory.query(function(suppliesresources) {
+                    $scope.suppliesresources = suppliesresources;
+                });
+
                 InitiateEventFactory.query(function(initiateevents) {
-                    $scope.initiateevents = initiateevents;
+                    //$scope.initiateevents = initiateevents;
+                    $scope.reservedSupplies = initiateevents[0].reserved_supplies;
+                    $scope.purchasedSupplies = initiateevents[0].purchased_supplies;
+                    //console.log($scope.initiateevents);
+                    //$scope.initiateevent.reserved_supplies = $scope.reserved_supplies;
                 });
             };
+         
         }
     ]);
+
 });
